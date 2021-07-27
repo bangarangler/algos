8b9db0b020ca2c085da664f2a7e9201a44ec486e
bangarangler <bangarangler@gmail.com>
2021-07-27 19:11:11 -0400

    wip

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -3,16 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	answer := fib(4)
+	answer := fib(6)
+	// answer := fib()
+	// fmt.Println("answer", answer(), answer(), answer(), answer(), answer())
 	fmt.Println("answer", answer)
 }
 
 func fib(num int) int {
 	seq := []int{1, 1}
-	fmt.Println("seq", seq)
-	for i := num; i >= 0; i-- {
-		fmt.Println("testing")
-		fmt.Println("i", i)
+	for i := 0; i < num; i++ {
+		fmt.Println("seq", seq)
+		seq = append(seq, (seq[i] + seq[i+1]))
+		// 1 + 1 = 2
+		// 1 + 2 = 3
+		// 2 + 3 = 5
+		// 3 + 5 = 8
+		// 5 + 8 = 13
 	}
-	return 1
+	return seq[num]
 }
+
+// func fib() func() int {
+// 	a, b := 0, 1
+// 	return func() int {
+// 		a, b = b, a+b
+// 		return a
+// 	}
+// }

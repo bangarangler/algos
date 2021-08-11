4f4f8498cfa21d26e20a234d871f978134a9a34f
bangarangler <bangarangler@gmail.com>
2021-08-11 12:26:28 -0400

    added bitwise solution

--- isPowerOfTwo/isPowerOfTwo.js
+++ isPowerOfTwo/isPowerOfTwo.js
@@ -7,25 +7,41 @@
 //1. divide number and future division results by two, until 1 is reached and
 //check if the remainder is always 0
 
-function isPowerOfTwo(number) {
-  if (number < 1) {
-    return false;
-  }
-  let dividedNumber = number;
-  while (dividedNumber !== 1) {
-    // console.count("Executing...");
-    if (dividedNumber % 2 !== 0) {
-      return false;
-    }
-    dividedNumber = dividedNumber / 2;
-  }
-  return true;
-}
+// function isPowerOfTwo(number) {
+//   if (number < 1) {
+//     return false;
+//   }
+//   let dividedNumber = number;
+//   while (dividedNumber !== 1) {
+//     // console.count("Executing...");
+//     if (dividedNumber % 2 !== 0) {
+//       return false;
+//     }
+//     dividedNumber = dividedNumber / 2;
+//   }
+//   return true;
+// }
 
 // Best Case: number = 13 -> O(1)
 // Average Case: O(log n)
 // Worst Case: 112,899,906,842,624 => O(log2 n) or O(log n)
 // if grows at much slower pace probably logarithmic time
+
+// console.log("isPowerOfTwo(8) :>> ", isPowerOfTwo(8));
+// console.log("isPowerOfTwo(5) :>> ", isPowerOfTwo(5));
+// console.log("isPowerOfTwo(116) :>> ", isPowerOfTwo(16));
+// console.log("isPowerOfTwo(13) :>> ", isPowerOfTwo(13));
+// console.log("isPowerOfTwo(20) :>> ", isPowerOfTwo(20));
+
+// Bitwise black magic
+function isPowerOfTwo(number) {
+  if (number < 1) {
+    // 1
+    return false;
+  }
+  return (number & (number - 1)) === 0; // 1
+}
+// Constant Time O(1);
 
 console.log("isPowerOfTwo(8) :>> ", isPowerOfTwo(8));
 console.log("isPowerOfTwo(5) :>> ", isPowerOfTwo(5));
